e31b02df43108c130550eceae94fe53abb928dc9
Bharatkumar Kori <bharatkumargkori@gmail.com>
2024-01-25 19:53:35 +0530

    app modified

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import os
 from joblib import dump, load
+from scipy.special import boxcox1p
 
 
 st.title("Industrial Copper Price prediction")
@@ -26,36 +27,47 @@ with col1:
        'product_ref', 'delivery_dats']
 
     # Function to preprocess the input data
-    def preprocess_input(input_data):
-        # Perform any necessary preprocessing steps
-        # E.g., one-hot encoding, scaling, etc.
-        processed_data = input_data  # Placeholder for now
-        return processed_data
+    def preprocess_input(input_data, lambdas):
+    # Perform any necessary preprocessing steps
+        features = ['quantity tons', 'thickness', 'width']
+        processed_data = input_data.copy()  # Create a copy of the input data to avoid modifying the original data
+        for feature,lam in zip(features,lambdas):
+            if feature in input_data:
+                original_data = input_data[feature]
+                # transform data with previously fitted lambda
+                fitted_data = boxcox1p(original_data,lam)
+                # Replace the original feature data with the transformed data
+                processed_data[feature] = fitted_data
+        return list(processed_data.values())
+
+
 
     # Function to make predictions
     def predict_class(input_data):
-        processed_data = preprocess_input(input_data)
-        predictions = model.predict(processed_data)
+        lam = [0.16566176182102382,-0.15661990549881208,0.8944320487982019]
+        processed_data = preprocess_input(input_data,lam)
+        x=pd.DataFrame([processed_data])
+        st.write(x)
+        predictions = model.predict(x)
         return predictions
 
     # Streamlit app
     def main():
         # Set app title and description
         st.title("Prediction Model")
-        st.write("This app predicts the class (1 or 0) based on input features.")
 
         # Create input fields for feature values
         inputs = {}
         for feature in feature_names:
-            inputs[feature] = st.text_input(feature,key=f"name_1_{feature}")
-
+            inputs[feature] = st.number_input(feature,key=f"name_1_{feature}")
+        st.write(inputs)
         # Create a DataFrame with the input values
-        input_df = pd.DataFrame([inputs])
+        #input_df = pd.DataFrame([inputs])
 
         # Make predictions and display the results
         if st.button("Predict",key="predict_button_1"):
-            predictions = predict_class(input_df)
-            st.write("Predicted Class:", np.exp(predictions[0]))
+            predictions = predict_class(inputs)
+            st.write("Predicted price:", np.exp(predictions[0]))
 
     # Run the app
     if __name__ == '__main__':
